d069717de6869c194b9badb9e33c9a3176474e30
Olimpiu Seulean <seulean.olimpiu@gmail.com>
2022-06-26 20:55:49 +0300

    clean-uo ViewNotes view.

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="notes">
-
     <AddEditNote
       v-model="newNote"
       placeholder="Add a new note"
@@ -22,45 +21,42 @@
       :key="note.id"
       :note="note"
     />
-
   </div>
 </template>
 
 <script setup>
-
 /*
   imports
 */
 
-  import { ref } from 'vue'
-  import Note from '@/components/Notes/Note.vue'
-  import AddEditNote from '@/components/Notes/AddEditNote.vue'
-  import { useStoreNotes } from '@/stores/storeNotes'
-  import { useWatchCharacters } from '@/use/useWatchCharacters'
+import { ref } from 'vue';
+import Note from '@/components/Notes/Note.vue';
+import AddEditNote from '@/components/Notes/AddEditNote.vue';
+import { useStoreNotes } from '@/stores/storeNotes';
+import { useWatchCharacters } from '@/use/useWatchCharacters';
 
 /*
   store
 */
 
-  const storeNotes = useStoreNotes()
+const storeNotes = useStoreNotes();
 
 /*
   notes
 */
 
-  const newNote = ref('')
-  const addEditNoteRef = ref(null)
+const newNote = ref('');
+const addEditNoteRef = ref(null);
 
-  const addNote = () => {
-    storeNotes.addNote(newNote.value)
-    newNote.value = ''
-    addEditNoteRef.value.focusTextarea()
-  }
+const addNote = () => {
+  storeNotes.addNote(newNote.value);
+  newNote.value = '';
+  addEditNoteRef.value.focusTextarea();
+};
 
 /*
   watch characters
 */
 
-  useWatchCharacters(newNote)
-
+useWatchCharacters(newNote);
 </script>
